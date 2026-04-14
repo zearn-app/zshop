057be1ea71a30214b429e115c1157b27953b73c1
zearn-app <zoroz0673@gmail.com>
2026-04-14 17:59:07 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useApp } from "./app";
+import { useApp } from "../app";
 import { signOut } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 import { useSearchParams } from "next/navigation";
