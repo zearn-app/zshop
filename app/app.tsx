ebe35f6dc3d43542f1ff38e9f30a904d36e43a02
zearn-app <zoroz0673@gmail.com>
2026-04-14 17:58:16 +0530

    Update app.tsx

--- app/app.tsx
+++ app/app.tsx
@@ -32,7 +32,7 @@ export const useApp = () => {
     goTolanding: () => router.push(routes.LANDING),
     goToLogin: () => router.push(routes.LOGIN),
     goToRegister: () => router.push(routes.REGISTER),
-    goToHome: = (query: string = "") => {router.push(routes.HOME + query);},
+    goToHome: () = (query: string = "") => {router.push(routes.HOME + query);},
     goToDashboard: () => router.push(routes.DASHBOARD),
     goToCompare: () => router.push(routes.COMPARE),
 
